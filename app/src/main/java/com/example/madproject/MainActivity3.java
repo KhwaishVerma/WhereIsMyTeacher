a51d3afdddbdedb50eb23948dc0192c436a3847d
Khwaish <khwahishverma2312@gmail.com>
2026-09-17 20:37:43 +0530

    Added 5th page(Student Dashboard)

--- app/src/main/java/com/example/madproject/MainActivity3.java
+++ app/src/main/java/com/example/madproject/MainActivity3.java
@@ -1,6 +1,10 @@
 package com.example.madproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,14 @@ public class MainActivity3 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        b1=findViewById(R.id.button6);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity3.this, MainActivity.class);
+                startActivity(i);
+            }
         });
     }
 }
