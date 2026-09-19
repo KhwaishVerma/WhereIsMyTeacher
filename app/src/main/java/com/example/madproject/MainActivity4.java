e1a412500cb44fe35230cbc380b22141d47dc65f
Khwaish <khwahishverma2312@gmail.com>
2026-09-19 16:34:01 +0530

    All file frontend functionalities Corrected

--- app/src/main/java/com/example/madproject/MainActivity4.java
+++ app/src/main/java/com/example/madproject/MainActivity4.java
@@ -3,8 +3,9 @@ package com.example.madproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,22 +14,73 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity4 extends AppCompatActivity {
+
     Button b1;
+    Spinner department, designation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main4);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        b1=findViewById(R.id.button7);
+
+        department = findViewById(R.id.editTextText13);
+        designation = findViewById(R.id.editTextText14);
+
+        String[] departments = {
+                "Select Department",
+                "STME",
+                "SBM",
+                "SOC",
+                "SPTM",
+                "SOL"
+        };
+
+        String[] designations = {
+                "Select Designation",
+                "Professor",
+                "Associate Professor",
+                "Assistant Professor",
+                "Lecturer",
+                "Visiting Faculty"
+        };
+
+        ArrayAdapter<String> departmentAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                departments
+        );
+
+        departmentAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        department.setAdapter(departmentAdapter);
+
+        ArrayAdapter<String> designationAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                designations
+        );
+
+        designationAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        designation.setAdapter(designationAdapter);
+
+        b1 = findViewById(R.id.button7);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(MainActivity4.this, MainActivity.class);
+                Intent i = new Intent(MainActivity4.this, MainActivity.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/madproject/MainActivity7.java
+++ app/src/main/java/com/example/madproject/MainActivity7.java
@@ -1,9 +1,6 @@
 package com.example.madproject;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity6 extends AppCompatActivity {
-    Button availability, requestmeeting;
+public class MainActivity7 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main6);
+        setContentView(R.layout.activity_main7);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        availability=findViewById(R.id.availabilityButton);
-        requestmeeting=findViewById(R.id.buttonRequestMeeting);
-        requestmeeting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity6.this, request_meeting.class);
-                startActivity(i);
-            }
         });
     }
 }
