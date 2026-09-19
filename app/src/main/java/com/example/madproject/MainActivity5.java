86572707577fe2cd1fc09e2f3a74344a4c9b5c10
Khwaish <khwahishverma2312@gmail.com>
2026-09-19 12:40:50 +0530

    added find teacher

--- app/src/main/java/com/example/madproject/MainActivity5.java
+++ app/src/main/java/com/example/madproject/MainActivity5.java
@@ -1,6 +1,9 @@
 package com.example.madproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity5 extends AppCompatActivity {
-
+    Button findteacher, myrequests;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,21 @@ public class MainActivity5 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        findteacher=findViewById(R.id.buttonFindTeacher);
+        myrequests=findViewById(R.id.buttonMyRequests);
+        findteacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity5.this, MainActivity6.class);
+                startActivity(i);
+            }
+        });
+        myrequests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
